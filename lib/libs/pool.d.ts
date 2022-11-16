f3d0d542f3f8e3a75ea23898d0a07807251bb77c
Joey <joey.kheireddine@outlook.com>
2022-11-16 16:01:47 +0100

    upd: start working on pool class

---
+++ lib/libs/pool.d.ts
@@ -0,0 +1,7 @@
+declare class Pool {
+    constructor();
+    registerTask(): void;
+    addTask(): void;
+    getTasks(): void;
+}
+export default Pool;
---
+++ src/libs/pool.ts
@@ -0,0 +1,12 @@
+class Pool {
+    constructor(){}
+
+    // Register a method for a specific task
+    registerTask(){}
+    // Add a task to the pool, must refer to a specific method
+    addTask(){}
+    // Get pending tasks in the pool
+    getTasks(){}
+}
+
+export default Pool;
